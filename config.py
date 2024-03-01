428ee12107f1543fe79402ab0b06bfb73e992437
Andrew Hooker <andrewhooker2@gmail.com>
2024-03-01 01:35:16 -0600

    Removing unused items from config

--- config.py
+++ config.py
@@ -1,22 +1,15 @@
 import os
 import openai
-from dotenv import load_dotenv
-
-# Accessing env
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 # Chat
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_version = os.getenv("API_VERSION")
+openai.api_key = os.environ["OPENAI_API_KEY"]
+chat_model = os.environ["OPENAI_MODEL"]
 
 # Text Embedding
-deployment_name = os.getenv("DEPLOYMENT_NAME")
-chat_model = os.getenv("OPENAI_MODEL")
-text_embedding_model = os.getenv("TEXT_EMBEDDING_MODEL")
-openai_vector_dimension = 1536  # This changes depending on model
+text_embedding_model = os.environ["TEXT_EMBEDDING_MODEL"]
+openai_vector_dimension = 1536
 
 # Qdrant
-qdrant_url = os.getenv("QDRANT_URL")
-qdrant_api_key = os.getenv("CLUSTER_API_KEY")
-qdrant_collection = os.getenv("QDRANT_COLLECTION_NAME")
+qdrant_url = os.environ["QDRANT_URL"]
+qdrant_api_key = os.environ["QDRANT_CLUSTER_KEY"]
+qdrant_collection = "TEST"
