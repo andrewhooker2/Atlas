15a804fb35f3fc65042c4352ab0e233c3f13060b
Andrew Hooker <andrewhooker2@gmail.com>
2024-03-01 01:34:05 -0600

    Updating qdrant with a batch action for the text file, updating methods.

--- qdrant.py
+++ qdrant.py
@@ -1,8 +1,11 @@
+from openai.types.chat import ChatCompletion
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
-import uuid
-
+from embedding import gen_embedding
+from datetime import datetime
+import typing
 import config
+import uuid
 
 # Set up your client
 qdrant_client = QdrantClient(
@@ -22,66 +25,58 @@ def create_collection():
         distance=models.Distance.COSINE
     ),
 
-    my_collection = "my-collection"
-
     try:
         first_collection = qdrant_client.recreate_collection(
-            collection_name=my_collection,
+            collection_name=config.qdrant_collection,
             vectors_config=models.VectorParams(size=config.openai_vector_dimension, distance=models.Distance.COSINE)
         )
     except Exception as e:
         print(f"Collection Creation Error: {e}")
 
-    # Might try this:
-    # client.create_collection(
-    #     collection_name="{collection_name}",
-    #     vectors_config=models.VectorParams(size=100, distance=models.Distance.COSINE),
-    # )
 
+def create_points_batch(conversation: typing.List[typing.Dict[str, str]]):
+    try:
+        points = []
+        for item in conversation:
+            # Using this for a filler for now
+            embedding = [.01]
+            # Check for embedding in the message
+            if item['role'] == 'system':
+                continue
+            if 'embedding' not in item and item['role'] != 'system':
+                embedding = gen_embedding(item['content'])
 
-def create_points_batch(embedding, message_id):
-    uuid_value = uuid.uuid4()
-    qdrant_client.upsert(
-        collection_name=config.qdrant_collection,
-        points=models.Batch(
-            ids=uuid_value,
-            vectors=embedding.tolist()
-        )
-    )
+            points.append(
+                models.PointStruct(
+                    id=str(uuid.uuid4()),
+                    vector=embedding,
+                    payload={
+                        "role": item['role'],
+                        "message": item['content'],
 
-
-def create_point(embedding, point_id, message_id):
-    uuid_value = uuid.uuid4()
-
-    print("Embedding: ")
-    print(embedding)
-
-    qdrant_client.upsert(
-        collection_name=config.qdrant_collection,
-        points=[
-            models.PointStruct(
-                id=point_id,
-                vector=embedding,
-                payload={
-                    "response_id": message_id
-                }
+                    },
+                )
             )
-        ]
-    )
+        test = qdrant_client.upsert(
+            collection_name=config.qdrant_collection,
+            points=points
+        )
+
+    except Exception as e:
+        print(f"Collection Batching Error: {e}")
 
 
-def temp_create_point(embedding, point_id, message_id, ai_response):
-    uuid_value = uuid.uuid4()
-
+def create_point(embedding: list[float], response: ChatCompletion):
     qdrant_client.upsert(
         collection_name=config.qdrant_collection,
         points=[
             models.PointStruct(
-                id=point_id,
+                id=str(uuid.uuid4()),
                 vector=embedding,
                 payload={
-                    "response_id": message_id,
-                    "message": ai_response
+                    "message": response.choices[0].message.content,
+                    "role": response.choices[0].message.role,
+                    "time": datetime.now(),
                 }
             )
         ]
@@ -96,5 +91,4 @@ def search_similar_vectors(embedding):
         with_payload=True,
         score_threshold=0.8
     )
-
     return search
